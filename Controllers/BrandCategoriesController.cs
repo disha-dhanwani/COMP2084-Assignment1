8cc7b9f592a73e80e2ff7f53ed702d1cb603b4c6
Disha Dhanwani <200434069@student.georgianc.on.ca>
2020-10-23 19:37:29 +0400

    Generated Controllers and Views for Brand Category and Sneaker.

---
+++ Controllers/BrandCategoriesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SneakerBoxStore.Data;
+using SneakerBoxStore.Models;
+
+namespace SneakerBoxStore.Controllers
+{
+    public class BrandCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BrandCategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BrandCategories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BrandCategories.ToListAsync());
+        }
+
+        // GET: BrandCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brandCategory = await _context.BrandCategories
+                .FirstOrDefaultAsync(m => m.BrandCategoryId == id);
+            if (brandCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(brandCategory);
+        }
+
+        // GET: BrandCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BrandCategoryId,BrandName")] BrandCategory brandCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(brandCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brandCategory);
+        }
+
+        // GET: BrandCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brandCategory = await _context.BrandCategories.FindAsync(id);
+            if (brandCategory == null)
+            {
+                return NotFound();
+            }
+            return View(brandCategory);
+        }
+
+        // POST: BrandCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BrandCategoryId,BrandName")] BrandCategory brandCategory)
+        {
+            if (id != brandCategory.BrandCategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(brandCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BrandCategoryExists(brandCategory.BrandCategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brandCategory);
+        }
+
+        // GET: BrandCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var brandCategory = await _context.BrandCategories
+                .FirstOrDefaultAsync(m => m.BrandCategoryId == id);
+            if (brandCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(brandCategory);
+        }
+
+        // POST: BrandCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var brandCategory = await _context.BrandCategories.FindAsync(id);
+            _context.BrandCategories.Remove(brandCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BrandCategoryExists(int id)
+        {
+            return _context.BrandCategories.Any(e => e.BrandCategoryId == id);
+        }
+    }
+}
---
+++ Controllers/SneakersController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SneakerBoxStore.Data;
+using SneakerBoxStore.Models;
+
+namespace SneakerBoxStore.Controllers
+{
+    public class SneakersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SneakersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sneakers
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Sneakers.Include(s => s.BrandCategory);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Sneakers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sneaker = await _context.Sneakers
+                .Include(s => s.BrandCategory)
+                .FirstOrDefaultAsync(m => m.SneakerId == id);
+            if (sneaker == null)
+            {
+                return NotFound();
+            }
+
+            return View(sneaker);
+        }
+
+        // GET: Sneakers/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandCategoryId"] = new SelectList(_context.BrandCategories, "BrandCategoryId", "BrandName");
+            return View();
+        }
+
+        // POST: Sneakers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SneakerId,BrandCategoryId,ModelName,ReleaseYear,Description,Material,Price,Image,InStock")] Sneaker sneaker)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sneaker);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandCategoryId"] = new SelectList(_context.BrandCategories, "BrandCategoryId", "BrandName", sneaker.BrandCategoryId);
+            return View(sneaker);
+        }
+
+        // GET: Sneakers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sneaker = await _context.Sneakers.FindAsync(id);
+            if (sneaker == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandCategoryId"] = new SelectList(_context.BrandCategories, "BrandCategoryId", "BrandName", sneaker.BrandCategoryId);
+            return View(sneaker);
+        }
+
+        // POST: Sneakers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SneakerId,BrandCategoryId,ModelName,ReleaseYear,Description,Material,Price,Image,InStock")] Sneaker sneaker)
+        {
+            if (id != sneaker.SneakerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sneaker);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SneakerExists(sneaker.SneakerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandCategoryId"] = new SelectList(_context.BrandCategories, "BrandCategoryId", "BrandName", sneaker.BrandCategoryId);
+            return View(sneaker);
+        }
+
+        // GET: Sneakers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sneaker = await _context.Sneakers
+                .Include(s => s.BrandCategory)
+                .FirstOrDefaultAsync(m => m.SneakerId == id);
+            if (sneaker == null)
+            {
+                return NotFound();
+            }
+
+            return View(sneaker);
+        }
+
+        // POST: Sneakers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sneaker = await _context.Sneakers.FindAsync(id);
+            _context.Sneakers.Remove(sneaker);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SneakerExists(int id)
+        {
+            return _context.Sneakers.Any(e => e.SneakerId == id);
+        }
+    }
+}
