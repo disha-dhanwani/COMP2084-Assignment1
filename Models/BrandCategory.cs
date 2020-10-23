ebc54ec93b83e7798ed317e1ca083c9325652eec
Disha Dhanwani <200434069@student.georgianc.on.ca>
2020-10-23 15:54:04 +0400

    Add project files.

---
+++ Models/BrandCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SneakerBoxStore.Models
+{
+    public class BrandCategory
+    {
+        public int BrandCategoryId { get; set; }  //Primary Key
+        [Required]
+        public String BrandName { get; set; }
+
+        //public List<Sneaker> Sneakers { get; set; }
+
+    }
+}
