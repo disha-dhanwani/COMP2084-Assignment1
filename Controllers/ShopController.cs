362031f834ec60e468305a905f2cb4a23eb942c4
Disha Dhanwani <200434069@student.georgianc.on.ca>
2020-12-12 23:55:47 +0400

    Few View changes.

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -41,19 +41,32 @@ namespace SneakerBoxStore.Controllers
             
             var customerId = GetCustomerId();
 
-            var cart = new Cart
-            {
-                SneakerId = SneakerId,
-                Quantity = Quantity,
-                CustomerId = customerId,
-                DateCreated = DateTime.Now,
-                Price = price,
-                Size = Size
-            };
+            var cartItem = _context.Carts.SingleOrDefault(c => c.SneakerId == SneakerId && c.CustomerId == customerId);
 
-            //save to the carts table in db
-            _context.Carts.Add(cart);
-            _context.SaveChanges();
+            //If the specified product is not present in the cart, add it to cart (create new cart object)
+            //Else, just update the quantity of that product in the cart.
+            if (cartItem == null)
+            {
+                var cart = new Cart
+                {
+                    SneakerId = SneakerId,
+                    Quantity = Quantity,
+                    CustomerId = customerId,
+                    DateCreated = DateTime.Now,
+                    Price = price,
+                    Size = Size
+                };
+
+                //save to the carts table in db
+                _context.Carts.Add(cart);
+                _context.SaveChanges();
+            }
+            else
+            {
+                cartItem.Quantity += Quantity;
+                _context.Carts.Update(cartItem);
+                _context.SaveChanges();
+            }
 
             return RedirectToAction("Cart");
 
@@ -73,8 +86,36 @@ namespace SneakerBoxStore.Controllers
         public IActionResult Cart()
         {
             var cartItems = _context.Carts.Include(c => c.Sneaker).Where(c => c.CustomerId == HttpContext.Session.GetString("CustomerId"));
+
+            //calculate the total number of items in cart
+            var numberOfItems = (from c in cartItems
+                                 select c.Quantity).Sum();
+            HttpContext.Session.SetInt32("NumberOfItems", numberOfItems);
+            
             return View(cartItems);
         }
+
+        //GET: /Shop/RemoveFromCart/3 (any id)
+        public IActionResult RemoveFromCart(int id)
+        {
+            //Find the item and then remove it from cart. Redirect user back to Cart.
+            var cartItem = _context.Carts.Find(id);
+            if(cartItem != null)
+            {
+                _context.Carts.Remove(cartItem);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Cart");
+        }
+
+        //Checkout forms
+
+        //GET: /Shop/Checkout
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
 
     }
 }
